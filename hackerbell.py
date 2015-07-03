fe99cc477b0ed82e656f20f554ecfd1e00bcfd71
sandbox <appflood_sandbox@papayamobile.com>
2015-07-03 00:20:37 -0700

    fix

--- hackerbell.py
+++ hackerbell.py
@@ -4,8 +4,10 @@ monkey.patch_all()
 import urllib2
 import requests, json
 import util
+HACKERBELL_JOBS = ['job', 'hire', 'hiring', 'full-time', 'full time']
 HACKERBELL_KEYS = ['python', 'amazon', 'nginx', 'aws', 'china', 'amazon', 'performance', 'language', 'hack']
 HACKERBELL_RECEIVER = 'weiyi@papayamobile.com'
+HACKERBELL_GUEST = 'haozhang85@gmail.com'
 HACKERBELL_SNIPPET =""" 
 <h4 style="margin:0;margin-bottom:6px;margin-top:6px">
 <a style="font-size:14px;line-height:22px;font-weight:bold;text-decoration:none;color:#259;border:none;outline:none" href="%s" target="_blank">%s</a>&nbsp;&nbsp;</h4>
@@ -25,8 +27,8 @@ def fetch_story(id):
     r = urllib2.urlopen('https://hacker-news.firebaseio.com/v0/item/%s.json' % id).read()
     return json.loads(r)
 
-def notify(title='[Hackerbell]Hacker News Top 10', body='N/A'): 
-    util.send_email(title, body, HACKERBELL_RECEIVER)
+def notify(title='[Hackerbell]Hacker News Top 10', body='N/A', to=HACKERBELL_RECEIVER): 
+    util.send_email(title, body, to)
 
 def wrap(items=None):
     if not items:
@@ -44,6 +46,9 @@ def task1():
 
 def task2():
     task_new_key(keys=HACKERBELL_KEYS)
+
+def task3():
+    task_job_key(keys=HACKERBELL_JOBS)
 
 def fetch_top(n=500):
     top = fetch_top_ids()[:n]
@@ -68,6 +73,15 @@ def task_new_key(keys=None):
     items = filter_keys(items, keys)
     body = wrap(items)
     notify(title='[Hackerbell]New keys', body=body)
+
+def task_job_key(keys=None):
+    items1 = fetch_new(n=1000)
+    items2 = fetch_top(n=100)
+    items = items1 + items2
+    items = filter_keys(items, keys)
+    body = wrap(items)
+    notify(title='[Hackerbell]Jobs', body=body)
+    notify(title='[Hackerbell]Jobs', body=body, to=HACKERBELL_GUEST)
 
 def filter_keys(items, keys):
     if not keys:
